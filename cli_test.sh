40b153a48e5bd7c69d00ec0d18c85819c7f8f61d
Filippo Ranza <filipporanza@gmail.com>
2019-12-20 22:35:21 +0100

    add cli_test.sh: ensure that the cli behaves as expected

---
+++ cli_test.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+cargo install --path . --force
+
+if [[ "$CARGO_HOME" ]] ; then
+    TMP_PATH="$CARGO_HOME/bin"
+else
+    TMP_PATH="$HOME/.cargo/bin"
+fi
+
+export PATH="$PATH:$TMP_PATH"
+
+cd $(mktemp -d)
+
+echo "Remove some files"
+touch {a..z}.txt
+rmd {a..f}.txt
+
+for i in  {g..z}.txt ; do
+    [[ -e "$i" ]] || exit 1
+done
+
+
+echo "Remove duplicates"
+for i in {a..z} ; do
+    echo "TEST" > "$i"
+done
+
+rmd -d 
+COUNT=0
+for i in {a..z} ; do
+    [[ -e "$i" ]] && (( COUNT++ ))
+done
+
+[[ "$COUNT" == '1' ]] || exit 1
+
+echo "Remove duplicates in a different directory"
+
+mkdir -p "SOME/PATH"
+
+echo "test" > "SOME/FILE"
+echo "test" > "SOME/PATH/FILE_2"
+echo "test" > "SOME/FILE_3"
+echo "test" > "DUP_A"
+echo "test" > "DUP_B"
+
+rmd -d 'SOME'
+
+COUNT=0
+[[ -e "SOME/FILE" ]] && (( COUNT++ ))
+[[ -e "SOME/PATH/FILE_2" ]] && (( COUNT++ ))
+[[ -e "SOME/FILE_3" ]] && (( COUNT++ ))
+[[ -e "DUP_A" ]] || exit 
+[[ -e "DUP_B" ]] || exit 
+[[ "$COUNT" == '1' ]] || exit 1
+
+echo "Remove Directory"
+rmd -rf 'SOME'
+[[ -e "SOME" ]] && exit 1
+
+echo "Done"
