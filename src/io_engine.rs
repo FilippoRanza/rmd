4a5596e97d9ec8327c4b1398b8903e83c62f53ad
Filippo Ranza <filipporanza@gmail.com>
2020-09-11 10:29:03 +0200

    add answer advice

--- src/io_engine.rs
+++ src/io_engine.rs
@@ -3,7 +3,7 @@ use std::io::*;
 pub fn remove_question(name: &str) -> Result<bool> {
     let mut buffer = String::new();
 
-    println!("remove: {}?", name);
+    println!("remove: {}? [y/N]", name);
     std::io::stdin().read_line(&mut buffer)?;
 
     let lower = buffer.to_lowercase();
